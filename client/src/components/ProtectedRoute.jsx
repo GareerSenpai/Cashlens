469782b235bbbefc4b04c3c6f1f7ca5a6e130b31
Anurag Bhattacharjee <anuragbhattacharjee612@gmail.com>
2025-10-20 15:32:00 +0530

    Fixed issue where user could view protected pages even when token expired

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -4,44 +4,30 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
-// const ProtectedRoute = ({ children }) => {
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-//   useEffect(() => {
-//     const checkAuth = async () => {
-//       try {
-//         const res = await axios.get(USER_URLS.userDetails, {
-//           withCredentials: true,
-//         });
-//         if (res.status === 200) {
-//           setIsAuthenticated(true);
-//         } else {
-//           setIsAuthenticated(false);
-//         }
-//       } catch (error) {
-//         setIsAuthenticated(false);
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     };
-
-//     checkAuth();
-//   }, []);
-
-//   if (isLoading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (!isAuthenticated) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   return children;
-// };
-
 const ProtectedRoute = ({ children }) => {
-  const { token, isLoading } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { token, setToken } = useAuth();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await axios.get(USER_URLS.userDetails, {
+          withCredentials: true,
+        });
+
+        if (res.status !== 200) {
+          setToken(null);
+        }
+      } catch (error) {
+        setToken(null);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
 
   if (isLoading) {
     return <div>Loading...</div>;
