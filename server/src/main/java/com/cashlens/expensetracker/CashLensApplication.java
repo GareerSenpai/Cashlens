7ca69fcc80b868c0c16000fae572f7fb37bf8df3
Anurag Bhattacharjee <anuragbhattacharjee612@gmail.com>
2025-10-26 11:57:44 +0530

    dotenv library setup

--- server/src/main/java/com/cashlens/expensetracker/CashLensApplication.java
+++ server/src/main/java/com/cashlens/expensetracker/CashLensApplication.java
@@ -3,10 +3,16 @@ package com.cashlens.expensetracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class CashLensApplication {
 
 	public static void main(String[] args) {
+		// Load .env variables into system properties before Spring starts
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
 		SpringApplication.run(CashLensApplication.class, args);
 	}
 
