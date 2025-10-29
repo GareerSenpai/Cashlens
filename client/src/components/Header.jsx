ebe35fafd3cdd7db4c362469f89260c844a69a54
Anurag Bhattacharjee <anuragbhattacharjee612@gmail.com>
2025-10-29 13:33:49 +0530

    Updated header style

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,8 +3,15 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="py-4 flex items-center bg-white">
-      <Link to="/" className="text-2xl font-medium px-4">
+    <div className="px-8 py-4 flex items-center bg-white">
+      <style>{`
+      .gradient-text {
+          background: linear-gradient(135deg, #1a1a1a, #8B5CF6);
+          -webkit-background-clip: text;
+          -webkit-text-fill-color: transparent;
+          background-clip: text;
+        }`}</style>
+      <Link to="/" className="text-3xl font-bold gradient-text">
         Cash Lens
       </Link>
     </div>
